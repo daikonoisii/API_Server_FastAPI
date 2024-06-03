4a48a32144ce257c593d2ff42c108aeb16a345ce
Ishizawa <dishizawa2001@gmail.com>
2024-06-04 06:54:30 +0900

    自動でenvファイルが生成されるように記述

--- script/up.sh
+++ script/up.sh
@@ -1,2 +1,11 @@
-docker-compose -f docker-compose.prod.yml build
-docker compose -f docker-compose.prod.yml up -d
+# api.env ファイルが存在しない場合にのみ新規作成
+if [ ! -f api.env ]; then
+    touch api.env
+    echo 'LOG_FILE_NAME="api.log"' >> api.env
+fi
+
+docker compose build
+docker compose up
+
+#docker-compose -f docker-compose.prod.yml build
+#docker compose -f docker-compose.prod.yml up -d
